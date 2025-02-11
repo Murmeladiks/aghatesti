da4975f474d78c3f63bd6977630083e4a55070ae
Joni Notkola <122889096+JNotkola@users.noreply.github.com>
2025-02-12 01:21:10 +0200

    asdASD
    ASSFVGsadeB

---
+++ scripts/vscripts/heroes/snapfire/modifier_snapfire_scatterblast_barrage.lua
@@ -0,0 +1,5 @@
+modifier_snapfire_scatterblast_barrage = class({})
+
+function modifier_snapfire_scatterblast_barrage:IsHidden() return true end
+function modifier_snapfire_scatterblast_barrage:IsPurgable() return false end
+function modifier_snapfire_scatterblast_barrage:RemoveOnDeath() return false end
